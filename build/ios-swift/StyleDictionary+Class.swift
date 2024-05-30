aeeeceae6beb8cec2e7d9d38caafb4bdc59c64b7
Flexowitsch <github@felixpfahlert.com>
2024-05-30 11:32:21 +0200

    c cc c c cc cchanges

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,18 +4,18 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 30 May 2024 09:02:00 GMT
+// Generated on Thu, 30 May 2024 09:31:57 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let demoBackgroundColor = #F0F0F8
-    public static let demoFontAccent = #866CEE
-    public static let demoFontDefault = #211A3
+    public static let demoBackgroundColor = #000
+    public static let demoFontAccent = #0000ff
+    public static let demoFontDefault = #fff
     public static let demoFontFamily = Cabinet Grotestk
-    public static let demoFontSizeLarge = 90
-    public static let demoFontSizeSmall = 48
+    public static let demoFontSizeLarge = 90px
+    public static let demoFontSizeSmall = 48px
     public static let demoFontWeightDefault = Regular
     public static let demoTest = #fff
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,18 +4,18 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 30 May 2024 09:02:00 GMT
+// Generated on Thu, 30 May 2024 09:31:57 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let demoBackgroundColor = #F0F0F8
-    internal static let demoFontAccent = #866CEE
-    internal static let demoFontDefault = #211A3
+    internal static let demoBackgroundColor = #000
+    internal static let demoFontAccent = #0000ff
+    internal static let demoFontDefault = #fff
     internal static let demoFontFamily = Cabinet Grotestk
-    internal static let demoFontSizeLarge = 90
-    internal static let demoFontSizeSmall = 48
+    internal static let demoFontSizeLarge = 90px
+    internal static let demoFontSizeSmall = 48px
     internal static let demoFontWeightDefault = Regular
     internal static let demoTest = #fff
 }
