178ddcb4b41a32f7f4a5b7eaf9dd206c4c522131
Flexowitsch <github@felixpfahlert.com>
2024-05-30 11:36:55 +0200

    push to fig

--- build/ios-swift/StyleDictionary+Enum.swift
+++ build/ios-swift/StyleDictionary+Enum.swift
@@ -4,16 +4,16 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 30 May 2024 09:31:57 GMT
+// Generated on Thu, 30 May 2024 09:35:17 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let demoBackgroundColor = #000
-    public static let demoFontAccent = #0000ff
-    public static let demoFontDefault = #fff
-    public static let demoFontFamily = Cabinet Grotestk
+    public static let demoBackgroundColor = #F0F0F8
+    public static let demoFontAccent = #866CEE
+    public static let demoFontDefault = #211A38
+    public static let demoFontFamily = Times New Roman
     public static let demoFontSizeLarge = 90px
     public static let demoFontSizeSmall = 48px
     public static let demoFontWeightDefault = Regular
